1e43bf177a231fbf328792ab80def02bc98a778e
PC <ohman.mattias@outlook.com>
2024-11-25 12:58:57 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADO.NetMotSakila_GOhman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
